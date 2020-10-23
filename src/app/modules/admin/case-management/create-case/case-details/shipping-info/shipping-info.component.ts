f9ff900ba7f97cca2d3b17e43011648bf8f958e5
Majid Ali Khan <majidalikhan1158@gmail.com>
2020-10-23 12:26:36 +0500

    small fixes

--- src/app/modules/admin/case-management/create-case/case-details/shipping-info/shipping-info.component.ts
+++ src/app/modules/admin/case-management/create-case/case-details/shipping-info/shipping-info.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { ShipmentTypes } from 'src/app/modules/shared/enums/shipment-types';
 import { MatSelectChange, MatSelect } from '@angular/material/select';
 import { ExpansionIcons } from 'src/app/modules/shared/enums/dynamic-icons';
+import { FormControl } from '@angular/forms';
 
 export interface ShipmentTypesBox {
   boxId: number;
   shipmentId: number;
   shipmentType: string;
+  isShipmentModeInternalTransfer: boolean;
+  isShipmentModeLibrary: boolean;
 }
 
 @Component({
@@ -16,16 +19,20 @@ export interface ShipmentTypesBox {
   encapsulation: ViewEncapsulation.None
 })
 export class ShippingInfoComponent implements OnInit {
-  @ViewChild('addShipmentSelectBox') addShipmentSelectBox: MatSelect;
   rowsToDisplay = [1];
   shipmentsToDisplay: ShipmentTypesBox[] = [];
   shipmentTypesArray = ShipmentTypes;
   ExpansionIcons =  ExpansionIcons;
   shouldShowShipmentDetails = false;
   boxIdToExpand = 0;
+  isShipmentModeLibrary = false;
+  isShipmentModeInternalTransfer = false;
+  shipmentSelectedTypeFormControl: FormControl;
+  selectedShipmentType: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.shipmentSelectedTypeFormControl = new FormControl(this.selectedShipmentType);
   }
 
   addRow() {
@@ -38,9 +45,11 @@ export class ShippingInfoComponent implements OnInit {
     this.rowsToDisplay.splice(rowIndex, 1);
   }
 
-  handleAddShipment(event: MatSelectChange) {
+  handleShipmentTypeChange(event: MatSelectChange) {
     const isShipmentExist = this.shipmentsToDisplay.find(x => x.shipmentId === event.value);
     if (isShipmentExist) {
+      this.selectedShipmentType = null;
+      this.shipmentSelectedTypeFormControl.setValue(this.selectedShipmentType);
       return;
     }
 
@@ -52,12 +61,14 @@ export class ShippingInfoComponent implements OnInit {
       this.shipmentsToDisplay.push({
         boxId: totalShipments + 1,
         shipmentId: shipmentToBeAdded.value,
-        shipmentType: shipmentToBeAdded.text
+        shipmentType: shipmentToBeAdded.text,
+        isShipmentModeInternalTransfer: false,
+        isShipmentModeLibrary: false
       });
-      this.boxIdToExpand = totalShipments + 1;
-      this.shouldShowShipmentDetails = true;
+      this.shouldShowShipmentDetails = false;
+      this.selectedShipmentType = null;
+      this.shipmentSelectedTypeFormControl.setValue(this.selectedShipmentType);
     }
-    this.addShipmentSelectBox.value.reset();
   }
 
   displayShipmentDetails(shipmentId) {
@@ -73,5 +84,23 @@ export class ShippingInfoComponent implements OnInit {
 
   deleteShipment(shipmentId) {
     this.shipmentsToDisplay = this.shipmentsToDisplay.filter(x => x.shipmentId !== shipmentId);
+  }
+
+  handleShipmentModeChange(event: MatSelectChange, shipmentId: number) {
+    this.shipmentsToDisplay.forEach(element => {
+      if (element.shipmentId === shipmentId) {
+        if (event.value === 'Internal Transfer (ML3PL)' || event.value === 'Internal Transfer (MPM3PL)') {
+          element.isShipmentModeInternalTransfer = true;
+          element.isShipmentModeLibrary = false;
+        } else if (event.value === 'Internal Transfer (Library)') {
+          element.isShipmentModeInternalTransfer = false;
+          element.isShipmentModeLibrary = true;
+        }
+      }
+    });
+  }
+
+  compare(c1: {name: string}, c2: {name: string}) {
+    return c1 && c2 && c1.name === c2.name;
   }
 }

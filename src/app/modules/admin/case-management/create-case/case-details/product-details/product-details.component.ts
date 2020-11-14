c4a1e4b67a65053fc9238811bf2a07775e653fe5
Majid Ali Khan <majidalikhan1158@gmail.com>
2020-11-14 20:05:09 +0500

    fixes

--- src/app/modules/admin/case-management/create-case/case-details/product-details/product-details.component.ts
+++ src/app/modules/admin/case-management/create-case/case-details/product-details/product-details.component.ts
@@ -272,42 +272,38 @@ export class ProductDetailsComponent implements OnInit, OnDestroy, OnChanges {
   handleAddFGRequiredEvent = (fgList: ProductDetailModals[]) => {
     let total = 0;
     if (fgList.length > 0) {
-      //   fgList.forEach(item => {
-      //     // tslint:disable-next-line: radix
-      //     total = parseInt(total.toString()) + parseInt(item.quantity.toString());
-      //   });
-      this.productDetailsVMList.forEach(item => {
+        fgList.forEach(item => {
+          // tslint:disable-next-line: radix
+          total = parseInt(total.toString()) + parseInt(item.quantity.toString());
+        });
+        this.productDetailsVMList.forEach(item => {
         if (item.id === this.recordIdPassToModal) {
           item.productISBNDetail.fgList = fgList;
           item.productISBNDetail.fGRequired = total;
-          // // tslint:disable-next-line: radix
-          // item.prodQty = parseInt(item.orderQty.toString()) + parseInt(total.toString());
-          // item = this.calculateSubTotal(item);
         }
       });
-      this.pushToStore();
+        this.pushToStore();
     }
     this.recordIdPassToModal = 0;
+    this.store.setProductDetailsId(0);
   }
 
   handleAddAdvanceRequiredEvent = (advancesList: ProductDetailModals[]) => {
     let total = 0;
     if (advancesList.length > 0) {
-      // advancesList.forEach(item => {
-      //   // tslint:disable-next-line: radix
-      //   total = parseInt(total.toString()) + parseInt(item.quantity.toString());
-      // });
+      advancesList.forEach(item => {
+        // tslint:disable-next-line: radix
+        total = parseInt(total.toString()) + parseInt(item.quantity.toString());
+      });
       this.productDetailsVMList.forEach(item => {
         if (item.id === this.recordIdPassToModal) {
           item.productISBNDetail.advancesList = advancesList;
           item.productISBNDetail.advancesRequired = total;
-          // tslint:disable-next-line: radix
-          // item.prodQty = parseInt(item.orderQty.toString()) + parseInt(total.toString());
-          // item = this.calculateSubTotal(item);
         }
       });
       this.pushToStore();
     }
     this.recordIdPassToModal = 0;
+    this.store.setProductDetailsId(0);
   }
 }

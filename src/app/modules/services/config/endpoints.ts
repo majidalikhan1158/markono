6fd82a659179a11c16797f160fc822ac151e8fb8
hafsa56 <hafsa1340@yahoo.com>
2020-12-10 11:35:36 +0500

    only the function calling structure and service api end points done

--- src/app/modules/services/config/endpoints.ts
+++ src/app/modules/services/config/endpoints.ts
@@ -12,12 +12,13 @@ export const Endpoints = {
         getShipmentTerm: 'ShippingTerms/GetShippingTerms',
         getShipmentAgent: 'ShippingAgent/GetShippingAgent',
         getCustomerDetail: 'https://cors-anywhere.herokuapp.com/http://app.markono.com/PODUAT/Case/GetCustomerData',
-        createCase : 'Case/CreateCase'
+        createCase: 'Case/CreateCase'
 
     },
     product: {
         getLiveVersion: 'GetLiveVersion',
-        getProductVersions: 'GetProductVersions'
+        getProductVersions: 'GetProductVersions',
+        getProducts: 'getProducts'
     },
     shopFloor: {
         getMachinesList: 'machines?filter[active]=true&filter[sdcFlag]=true&sort=seq'

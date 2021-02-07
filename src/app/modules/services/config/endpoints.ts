43cb6e5d76ae8b98ceb783a209c8bf3acfeba04d
Majid Ali Khan <majidalikhan1158@gmail.com>
2021-02-07 12:37:36 +0500

    Update endpoints.ts

--- src/app/modules/services/config/endpoints.ts
+++ src/app/modules/services/config/endpoints.ts
@@ -12,7 +12,7 @@ export const Endpoints = {
         getShipmentMode: 'ShipmentMode/GetShipmentMode',
         getShipmentTerm: 'ShippingTerms/GetShippingTerms',
         getShipmentAgent: 'ShippingAgent/GetShippingAgent',
-        getCustomerDetail: 'https://cors-anywhere.herokuapp.com/http://app.markono.com/PODUAT/Case/GetCustomerData',
+        getCustomerDetail: 'http://app.markono.com/PODUAT/Case/GetCustomerData',
         createCase: 'Case/CreateCase',
         createShipment: 'Shipment/CreateShipment'
 
